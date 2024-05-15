291d8411920cd3228de5b40620897d60bba6e831
Tom Nil <tnil1143@gmail.com>
2024-05-15 10:42:20 +0000

    commit

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fy7&%idm=knl4$qm4^@ffq6ai@kmshdwaz=vlq#csum@egp)rt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-nil1143-djangoproject-mrktjek8m3n.ws-eu111.gitpod.io','.herokuapp.com']
 
